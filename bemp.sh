96c5b69608b5ee04122957f49c325fbdb6873a35
Sabbir Ahmed <sabbiirr@gmail.com>
2015-06-04 17:41:06 +0600

    Tapping dupes and versions

--- bemp.sh
+++ bemp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo -e "\n"
 echo -e "Welcome to BEMP installation script"
 echo -e "by sabbir"
 echo -e "https://github.com/thesabbir/bemp"
@@ -10,8 +11,15 @@ brew update
 echo -e "\nTapping \"homebrew-php\""
 brew tap homebrew/homebrew-php
 
+echo -e "\nTapping \"homebrew/versions\""
+brew tap homebrew/versions
+
+echo -e "\nTapping \"homebrew/dupes\""
+brew tap homebrew/dupes
+
 echo -e "\nInstalling PHP5.6 (without apache)"
 brew install php56 --with-fpm  --with-imap  --without-apache --with-debug
+
 echo -e "\nInstalling php56-mcrypt"
 brew install php56-mcrypt
 
